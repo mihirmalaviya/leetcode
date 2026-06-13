04918b45d8928a67a27a0d29c73d43af62450214
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-06-13 20:28:33 +0000

    [LeetCode Sync] Runtime - 23 ms (93.21%), Memory - 30.9 MB (86.42%)

--- 0187-repeated-dna-sequences/solution.py
+++ 0187-repeated-dna-sequences/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        if len(s)<10: return []
+        if len(s)<10:
+            return []
         
+        seen=set()
         res=set()
-        seen = set()
-        for i in range(len(s)-9):
-            sub=s[i:i+10:]
-            if sub in seen: res.add(sub)
-            seen.add(sub)
+        for i in range(len(s)-10+1):
+            subseq=s[i:i+10]
+            if subseq in seen:
+                res.add(subseq)
+            seen.add(subseq)
 
         return list(res)
         
