97c35d2be7af148436f7894a29e1c92bdcef3112
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-06-13 20:11:45 +0000

    [LeetCode Sync] Runtime - 47 ms (87.49%), Memory - 19.8 MB (23.76%)

--- 0387-first-unique-character-in-a-string/solution.py
+++ 0387-first-unique-character-in-a-string/solution.py
@@ -1,19 +1,9 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        
-        q=deque()
-        seen=set()
-        repeated=set()
-        for i in range(len(s)):
-            ch = s[i]
-            if ch not in seen:
-                q.append((ch,i))
-                seen.add(ch)
-            else:
-                repeated.add(ch)
-            
-        while q and q[0][0] in repeated:
-            q.popleft()
 
-        return -1 if not q else q[0][1]
+        counts=Counter(s)
+        for i in range(len(s)):
+            if counts[s[i]]==1:
+                return i
+        return -1
 
