7c4407eee0b3f28b7a8cdaff4a67ee2a9b0ef436
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-06-16 02:54:46 +0000

    [LeetCode Sync] Runtime - 8 ms (34.82%), Memory - 21.7 MB (13.10%)

---
+++ 0532-k-diff-pairs-in-an-array/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        pairs = set()
+        seen = set()
+        for n in nums:
+            if n-k in seen:
+                pairs.add((n-k,n))
+            if n+k in seen:
+                pairs.add((n,n+k))
+
+            seen.add(n)
+        return len(pairs)
+
