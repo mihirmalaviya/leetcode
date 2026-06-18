10e1d0a1649f40e0e889160616a2e51858e5ceab
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-06-18 17:42:19 +0000

    [LeetCode Sync] Runtime - 40 ms (40.84%), Memory - 31.5 MB (44.84%)

--- 0053-maximum-subarray/solution.py
+++ 0053-maximum-subarray/solution.py
@@ -1,32 +1,9 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        # best_sum = float('-inf')
-        # curr_sum = 0
-        # l,r=0,0
-        # while r<len(nums):
-        #     curr_sum += nums[r]
-        #     r+=1
-
-        #     best_sum = max(best_sum,curr_sum)
-
-        #     if r!=len(nums) and curr_sum < 0: 
-        #         while l<r:
-        #             curr_sum -= nums[l]
-        #             l+=1
-                
-        # best_sum = max(best_sum,curr_sum)
-
-        # return best_sum
-
-        # Kadanes algo
-        curr_max, max_till_now = 0, float('-inf')
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res=float('-inf')
+        currmax=0
         for n in nums:
-            curr_max = max(n, curr_max+n)
-            max_till_now = max(curr_max, max_till_now)
-            
-        return max_till_now
+            currmax=max(n,currmax+n)
+            res=max(currmax,res)
+        return res
+        
