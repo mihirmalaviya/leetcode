18ef9339f208e4e7ce99abd249db9f5264bf9f69
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-06-18 16:08:14 +0000

    [LeetCode Sync] Runtime - 50 ms (52.92%), Memory - 28.8 MB (11.48%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,25 +1,9 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        
-        curr_min = prices[0]
-        max_profit = 0
-        for price in prices[1:]:
-            if price<curr_min:
-                curr_min = price
-            else:
-                max_profit = max(max_profit, price-curr_min)
-
-        return max_profit
-
-
-"""
-7,1,5,3,6,4
-  ^ ^
-first min
-first max
-
-"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        currmin=float('inf')
+        res=0
+        for p in prices:
+            currmin=min(p,currmin)
+            res=max(res, p-currmin)
+        return res
+ 
