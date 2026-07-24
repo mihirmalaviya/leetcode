9d7d1d57da303e9828faf898f0093210c60f038e
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-07-24 23:28:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (73.51%)

---
+++ 0014-longest-common-prefix/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        ans=[]
+        strs=sorted(strs)
+
+        first=strs[0]
+        last=strs[-1]
+        for i in range((min(len(first),len(last)))):
+            if first[i]!=last[i]:
+                return "".join(ans)
+
+            ans+=first[i]
+        return "".join(ans)
+
