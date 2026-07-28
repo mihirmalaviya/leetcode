b82a120e1dc8c0c438e07ac374f0ca2de53f00b3
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-07-28 02:43:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.9 MB (89.34%)

---
+++ 0102-binary-tree-level-order-traversal/solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        if not root: return []
+        
+        res=[]
+        q=deque([root])
+        while q:
+            res.append([])
+            for _ in range(len(q)):
+                n=q.popleft()
+                res[-1].append(n.val)
+                if n.left: q.append(n.left)
+                if n.right: q.append(n.right)
+
+        return res
