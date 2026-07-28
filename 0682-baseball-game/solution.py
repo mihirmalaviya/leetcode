c4d46a9c04247575e696f4f81b6abfcc01599554
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-07-28 23:48:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (9.71%)

--- 0682-baseball-game/solution.py
+++ 0682-baseball-game/solution.py
@@ -1,16 +1,18 @@
-class Solution:
-    def calPoints(self, operations: List[str]) -> int:
+class Solution(object):
+    def calPoints(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
         
-
         s=[]
         for o in operations:
             if o=="C":
                 s.pop()
             elif o=="D":
-                s.append(s[-1]*2)
+                s.append(2*s[-1])
             elif o=="+":
                 s.append(s[-2]+s[-1])
             else:
                 s.append(int(o))
-            print(s)
         return sum(s)
