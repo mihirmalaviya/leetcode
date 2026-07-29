e48a35189b08bd9fce8a0bf21686db0cb43998ef
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-07-29 18:30:30 +0000

    [LeetCode Sync] Runtime - 76 ms (82.49%), Memory - 19.4 MB (97.74%)

--- 0424-longest-repeating-character-replacement/solution.py
+++ 0424-longest-repeating-character-replacement/solution.py
@@ -1,44 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
-        res=0
-        freq = [set() for _ in range(len(s))]
-        counts = defaultdict(int)
+        freq=defaultdict(int)
         maxfreq=0
+        res=0
 
         l=0
         for r in range(len(s)):
-            
-            counts[s[r]]+=1
-            c=counts[s[r]]
-            maxfreq=max(maxfreq,c)
-            freq[c-1].add(s[r])
+            freq[s[r]]+=1
+            maxfreq=max(freq[s[r]], maxfreq)
 
-            total=r-l+1
-            if k>=total-maxfreq:
-                res=max(res,total)
-            
-            # print(total,maxfreq,freq)
-
-            while l<r and not k>=total-maxfreq:
-                # print(total,maxfreq)
-                c=counts[s[l]]
-                counts[s[l]]-=1
-
-                freq[c-1].discard(s[l])
-                if not freq[c-1]:
-                    maxfreq-=1
-
-                total=l-r+1
+            while (r-l+1)-maxfreq>k:
+                freq[s[l]]-=1
                 l+=1
 
+            res=max(res,r-l+1)
         return res
-                    
-            
-            
 
-'''
-keep a sliding counter
-k must be <= total-max
-we have a freqs array of sets to store freqs
-'''
+
