70e68a7b833e1434239361ecfeb0d6ff470befb9
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-08-01 06:35:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (22.53%)

--- 0062-unique-paths/solution.py
+++ 0062-unique-paths/solution.py
@@ -1,24 +1,13 @@
-class Solution(object):
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-
-        def isValid(y,x):
-            if y>=m: return 0
-            if x>=n: return 0
-            return 1
-
-        dp=[[0]*n for _ in range(m)]
-        dp[m-1][n-1]=1
-        for y in reversed(range(m)):
-            for x in reversed(range(n)):
-                if isValid(y+1,x):
-                    dp[y][x]+=dp[y+1][x]
-                if isValid(y,x+1):
-                    dp[y][x]+=dp[y][x+1]
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         
-        # print(dp)
-        return dp[0][0]
+
+        dp=[[1]*n for _ in range(m)]
+
+        for y in range(1,m):
+            for x in range(1,n):
+                dp[y][x]=dp[y-1][x]+dp[y][x-1]
+
+        return dp[-1][-1]
+
+
