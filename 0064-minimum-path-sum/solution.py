1806eefe9106cc60a655d1fa5f311bae35533468
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-08-04 05:04:12 +0000

    [LeetCode Sync] Runtime - 7 ms (91.82%), Memory - 21.7 MB (47.96%)

--- 0064-minimum-path-sum/solution.py
+++ 0064-minimum-path-sum/solution.py
@@ -5,8 +5,9 @@ class Solution:
         for x in range(1,len(grid[0])):
             grid[0][x]+=grid[0][x-1]
         
-        for y in (range(1,len(grid))):
-            for x in (range(1,len(grid[0]))):
+        for y in range(1,len(grid)):
+            for x in range(1,len(grid[0])):
                 grid[y][x]+=min(grid[y-1][x],grid[y][x-1])
-            
+
         return grid[-1][-1]
+
