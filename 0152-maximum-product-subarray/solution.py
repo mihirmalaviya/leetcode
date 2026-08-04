b5675c6239ac10c325b3678a11ff62fa61bd3426
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-08-04 04:36:23 +0000

    [LeetCode Sync] Runtime - 3 ms (90.46%), Memory - 19.8 MB (44.94%)

---
+++ 0152-maximum-product-subarray/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res=max(nums)
+        curmax=curmin=1
+
+        for n in nums:
+            temp=curmax*n
+            curmax=max(temp,curmin*n,n)
+            curmin=min(temp,curmin*n,n)
+
+            res=max(res,curmax)
+        
+        return res
