a0d93f635d851cc931a9eaf5149962c68e98a9ee
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-08-10 03:24:24 +0000

    [LeetCode Sync] Runtime - 108 ms (63.76%), Memory - 19.3 MB (90.78%)

--- 0494-target-sum/solution.py
+++ 0494-target-sum/solution.py
@@ -1,24 +1,28 @@
-class Solution(object):
-    def findTargetSumWays(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-
-        self.memo = {}
-        return self.dp(nums,target,len(nums)-1,0)
-
-    def dp(self,nums,target,i,curr_sum):
-        if (i,curr_sum) in self.memo:
-            return self.memo[(i,curr_sum)]
-        if i<0 and curr_sum==target:
-            return 1
-        if i<0: 
-            return 0
-
-        pos=self.dp(nums,target,i-1,curr_sum+nums[i])
-        neg=self.dp(nums,target,i-1,curr_sum-nums[i])
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
-        self.memo[(i,curr_sum)]=pos+neg
-        return pos+neg
+        dpi=defaultdict(int)
+        dpi[0]=1
+
+        for i in range(len(nums)):
+            dpi1=defaultdict(int)
+            for total, count in dpi.items():
+                dpi1[total-nums[i]] += count
+                dpi1[total+nums[i]] += count
+            dpi=dpi1
+
+        return dpi[target]
+
+
+        # @cache
+        # def dp(i,x):
+        #     if i==len(nums):
+        #         return 1 if x==0 else 0
+            
+        #     take=0
+        #     skip=dp(i+1,x+nums[i])
+        #     take=dp(i+1,x-nums[i])
+            
+        #     return skip+take
+
+        # return dp(0,target)
