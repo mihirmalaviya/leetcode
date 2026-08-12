4c9c0ab55d23c2c4427ec1b6eb231b46270eafc3
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-08-12 00:53:47 +0000

    [LeetCode Sync] Runtime - 240 ms (66.50%), Memory - 21.6 MB (73.03%)

--- 0200-number-of-islands/solution.py
+++ 0200-number-of-islands/solution.py
@@ -2,21 +2,23 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows,cols=len(grid),len(grid[0])
 
-        seen=[[False]*cols for _ in range(rows)]
-        res=0
+        def dfs(r,c):
+            if r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0":
+                return
+            grid[r][c]='0'
 
-        for y in range(rows):
-            for x in range(cols):
-                if seen[y][x] or grid[y][x]=='0': continue
-                res+=1
-                seen[y][x]=True
-                q=deque([(y,x)])
-                while q:
-                    yy,xx=q.popleft()
-                    for dy,dx in ((-1,0),(1,0),(0,-1),(0,1)):
-                        ny,nx=yy+dy,xx+dx
-                        if 0<=ny<rows and 0<=nx<cols and not seen[ny][nx] and grid[ny][nx]=='1':
-                            seen[ny][nx]=True
-                            q.append((ny,nx))
+            dfs(r+1,c)
+            dfs(r,c+1)
+            dfs(r-1,c)
+            dfs(r,c-1)
+            return
+        
+        res =0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=='1':
+                    dfs(r,c)
+                    res+=1
+
         return res
 
