1c540c502e544c866dcc6967f233b52661018e53
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-08-13 04:08:40 +0000

    [LeetCode Sync] Runtime - 258 ms (92.92%), Memory - 19.4 MB (93.25%)

--- 1250-longest-common-subsequence/solution.py
+++ 1250-longest-common-subsequence/solution.py
@@ -1,14 +1,37 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        h,w=len(text1),len(text2)
-        dp=[[0]*(w+1) for _ in range(h+1)]
 
-        for y in range(1,h+1):
-            for x in range(1,w+1):
-                if text1[y-1]==text2[x-1]:
-                    dp[y][x] = 1+dp[y-1][x-1]
-                    
-                dp[y][x] = max(dp[y][x],dp[y-1][x])
-                dp[y][x] = max(dp[y][x],dp[y][x-1])
-        
-        return dp[h][w]
+        n=len(text1)
+        m=len(text2)
+
+        dpi_1=[0]*(m+1)
+
+        for i in range(1,n+1):
+            dpi=[0]*(m+1)
+            for j in range(1,m+1):
+                if text1[i-1]==text2[j-1]:
+                    dpi[j] = 1+dpi_1[j-1]
+
+                else:
+                    dpi[j] = max(dpi_1[j], dpi[j-1])
+            dpi_1=dpi
+
+        return dpi[-1]
+
+'''
+
+        @cache
+        def dp(i,j):
+            if i<0 or j<0:
+                return 0
+            if text1[i]==text2[j]:
+                return 1+dp(i-1,j-1)
+            else:
+                return max(\
+                    dp(i-1,j),
+                    dp(i,j-1)
+                )
+            
+        return dp(len(text1)-1, len(text2)-1)
+
+'''
