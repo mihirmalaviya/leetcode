10c9ef6289cffad42fa3a6a4e7a1a3bbb70b7c7d
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-08-31 01:21:56 +0000

    [LeetCode Sync] Runtime - 4 ms (10.47%), Memory - 19.5 MB (67.13%)

--- 0046-permutations/solution.py
+++ 0046-permutations/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
 
+
         res=[]
 
-        def b(s):
-            if s==len(nums):
+        def b(x):
+            if x==len(nums):
                 res.append(nums[:])
+                
+            for i in range(x,len(nums)):
+                nums[i],nums[x]=nums[x],nums[i]
+                b(x+1)
+                nums[i],nums[x]=nums[x],nums[i]
 
-            for i in range(s,len(nums)):
-                nums[s],nums[i]=nums[i],nums[s]
-                b(s+1)
-                nums[s],nums[i]=nums[i],nums[s]
-        
+
         b(0)
         return res
-
