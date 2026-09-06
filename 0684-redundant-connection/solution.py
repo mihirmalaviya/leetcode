98aa26ba3d9b4225a7513004e8b3bda15fcc10c0
Mihir Malaviya <mihirmalaviya1@gmail.com>
2026-09-06 04:52:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.8 MB (17.83%)

--- 0684-redundant-connection/solution.py
+++ 0684-redundant-connection/solution.py
@@ -1,24 +1,23 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         
-        parent=[i for i in range(len(edges)+1)]
+        parent = [i for i in range(len(edges)+1)]
 
         def find(x):
             if parent[x]!=x:
                 parent[x]=find(parent[x])
-
+            
             return parent[x]
 
         def union(x,y):
-            a=find(x)
-            b=find(y)
-            if a==b:
-                return True
-            else:
+            a,b=find(x),find(y)
+            if a!=b:
                 parent[a]=b
                 return False
-        
+            return True
+            
         for a,b in edges:
             if union(a,b):
                 return [a,b]
+
 
